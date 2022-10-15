a739e9dd8add39ec7c258a7c856a59b6f9753c54
Ehsan <ehsan@gmail.com>
2022-10-15 17:35:21 +0500

    Initial commit

---
+++ app/src/main/java/com/android/canva/test/MainActivity.java
@@ -0,0 +1,14 @@
+package com.android.canva.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
